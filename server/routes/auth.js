9c4ea6cb4250a318c98670c3d4444ac639ef6cff
Alok <salok1877@gmail.com>
2026-05-23 17:46:53 +0530

    setup brevo smtp

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -12,15 +12,12 @@ const JWT_SECRET = process.env.JWT_SECRET || "fallback_secret_for_dev_only";
 
 // EMAIL TRANSPORTER
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    },
-    tls: {
-        rejectUnauthorized: false
     }
 });
 
